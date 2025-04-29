1462234c0ab2a2dde89b7e29b4a181924ef5055a
Aleksandr Garanin <a.garanin@pbfgroup.ru>
2025-04-29 19:17:43 +0300

    Второй вариант миграции

--- TestMgration/Entities/Migration/MigrationPlan.swift
+++ TestMgration/Entities/Migration/MigrationPlan.swift
@@ -12,22 +12,21 @@ struct MigrationPlan: SchemaMigrationPlan {
     static var schemas: [any VersionedSchema.Type] = [Schema100.self, Schema101.self, Schema102.self]
     static var stages: [MigrationStage] = [ stage100to101, stage101to102 ]
     
-    static let stage100to101: MigrationStage = MigrationStage.custom(
+    static let stage100to101 = MigrationStage.lightweight(
         fromVersion: Schema100.self,
-        toVersion: Schema101.self,
-        willMigrate: nil,
-        didMigrate: { context in
+        toVersion: Schema101.self
+    )
+    
+    static let stage101to102: MigrationStage = MigrationStage.custom(
+        fromVersion: Schema101.self,
+        toVersion: Schema102.self,
+        willMigrate: { context in
             let orders = try context.fetch(FetchDescriptor<Schema101.Order>())
             for order in orders{
                 order.closed = order.isClosed ? Date() : nil
             }
-            
             try context.save()
-        }
-    )
-    
-    static let stage101to102 = MigrationStage.lightweight(
-        fromVersion: Schema101.self,
-        toVersion: Schema102.self
+        },
+        didMigrate: nil
     )
 }
